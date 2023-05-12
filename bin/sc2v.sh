ee2a8788b2df32868137a34ad66ff3f606b61578
Александр Абраамян <samvelcc25@outlook.com>
2023-05-12 15:35:49 +0300

    new: translation stages are now printed

--- bin/sc2v.sh
+++ bin/sc2v.sh
@@ -5,7 +5,10 @@
 #
 # usage: sc2v <filename_without_extension>
 #
+echo Cpp file processing...
 ../bin/sc2v_step1 < $1.cpp
+echo Header file processing...
 ../bin/sc2v_step2 < $1.h > $1.sc2v
+echo SC2V file processing...
 ../bin/sc2v_step3 < $1.sc2v > $1.v
 \rm *.sc2v
